f4b0cd050e9bf1738c534e3fb3786b912f431156
童健 <tongjian03@meituan.com>
2018-11-16 10:58:20 +0800

    1

--- src/pages/webSocket/webSocket.jsx
+++ src/pages/webSocket/webSocket.jsx
@@ -1,9 +1,38 @@
 import React, { PureComponent } from 'react'
+import io from 'socket.io-client';
 
 
 
 
 export default class webSocket extends PureComponent {
+
+    state = {
+        socketIO: null,
+    }
+
+    componentDidMount() {
+        this.initSocket();
+    }
+
+    initSocket() {
+        const wsurl = 'ws://47.92.222.71:8001';
+        console.log(wsurl);
+
+        this.socketIO = io.connect(wsurl);
+        this.socketIO.emit('group', {
+            type: 'position',
+            data: 'webSocket',
+        });
+        this.socketIO.emit('message',{
+            type: 'text',
+            from: 'webSocket',
+            data: '123'
+        })
+        this.socketIO.on('webSocket',(data) => {
+            console.log(data);
+        })
+    }
+
     render(){
         return (
             <div>
